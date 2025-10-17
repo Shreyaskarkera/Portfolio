df081ae9b03b39bcf4b2b054d3e05b49680d544d
Shreyaskarkera <shreyasbkutlu@gmail.com>
2025-10-18 00:59:35 +0530

    fixing scroll bug

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,22 @@ import MoreAbout from './Components/MoreAbout'
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={
-          <div className=' md:mx-19 lg:mx-38 snap-x snap-mandatory'>
-            <Home className="snap-center" />
-            <About className="snap-center" />
-          </div>
-        } />
-        <Route path="/more-about" element={
-          <div className='mx-3 md:mx-19 lg:mx-38'>
-            <MoreAbout />
-          </div>
-        } />
-      </Routes>
+      <ScrollToTop>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <div className=' md:mx-19 lg:mx-38 snap-x snap-mandatory'>
+              <Home className="snap-center" />
+              <About className="snap-center" />
+            </div>
+          } />
+          <Route path="/more-about" element={
+            <div className='mx-3 md:mx-19 lg:mx-38'>
+              <MoreAbout />
+            </div>
+          } />
+        </Routes>
+      </ScrollToTop>
     </Router>
   )
 }
