9f3fc6cc21fd364d6757afd862dc2997fd90e354
Shreyaskarkera <shreyasbkutlu@gmail.com>
2025-10-17 15:46:26 +0530

    just checking

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,45 +4,45 @@ import NavigationButton from './NavigationButton';
 
 const Home = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-[#262626] text-white px-4 sm:px-6 md:px-12 lg:px-24">
+        <>
+            <div className="flex flex-col items-center justify-center min-h-screen bg-[#262626] text-white px-4 sm:px-6 md:px-12 lg:px-24">
 
-            {/* First Line */}
-            <motion.p
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-                className="text-4xl sm:text-6xl md:text-6xl lg:text-8xl font-light text-[var(--text-color)] mb-3 tracking-[0.10em] sm:tracking-[0.12em] md:tracking-[0.15em]"
-            >
-                CREATIVE
-            </motion.p>
+                {/* First Line */}
+                <motion.p
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className="text-4xl sm:text-6xl md:text-6xl lg:text-8xl font-light text-[var(--text-color)] mb-3 tracking-[0.10em] sm:tracking-[0.12em] md:tracking-[0.15em]"
+                >
+                    CREATIVE
+                </motion.p>
 
-            {/* Second Line */}
-            <motion.p
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.75, delay: 0.5 }}
-                className="text-4xl sm:text-6xl md:text-6xl lg:text-8xl font-semibold tracking-[0.18em] sm:tracking-[0.20em] md:tracking-[0.24em] text-[var(--maintext-color)]  mb-3 font-serif"
-            >
-                FRONTEND
-            </motion.p>
+                {/* Second Line */}
+                <motion.p
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.75, delay: 0.5 }}
+                    className="text-4xl sm:text-6xl md:text-6xl lg:text-8xl font-semibold tracking-[0.18em] sm:tracking-[0.20em] md:tracking-[0.24em] text-[var(--maintext-color)]  mb-3 font-serif"
+                >
+                    FRONTEND
+                </motion.p>
 
-            {/* Third Line */}
-            <motion.p
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 1 }}
-                className="text-4xl sm:text-6xl md:text-6xl lg:text-8xl font-light text-[var(--text-color)] tracking-[0.10em] sm:tracking-[0.12em] md:tracking-[0.15em]"
-            >
-                DEVELOPER
-            </motion.p>
+                {/* Third Line */}
+                <motion.p
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, delay: 1 }}
+                    className="text-4xl sm:text-6xl md:text-6xl lg:text-8xl font-light text-[var(--text-color)] tracking-[0.10em] sm:tracking-[0.12em] md:tracking-[0.15em]"
+                >
+                    DEVELOPER
+                </motion.p>
 
+            </div>
             {/* Navigation Button */}
             <div className="absolute bottom-4 sm:bottom-10 left-0 w-full flex justify-center px-2 sm:px-0 ">
                 <NavigationButton name='ABOUT' />
             </div>
-
-
-        </div>
+        </>
     )
 }
 
