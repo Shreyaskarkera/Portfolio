155d5fcbdb1f7584af450372b25777bf484d1267
Shreyaskarkera <shreyasbkutlu@gmail.com>
2025-10-17 13:55:03 +0530

    made some changes in resopnive designe

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,16 +7,16 @@ import { motion } from "framer-motion";
 const About = () => {
     return (
         <>
-            <div className="mt-24 px-4 sm:px-6 md:px-12 lg:px-24">
+            <div className="mt-[100px] px-4 sm:px-6 md:px-12 lg:px-24">
                 {/* ABOUT Heading */}
                 <motion.p
                     initial={{ opacity: 0, y: 20 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     viewport={{ once: false, amount: 0.4 }}
                     transition={{ duration: 0.6, ease: "easeOut" }}
-                    className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-light text-[var(--text-color)] mb-3 tracking-[0.10em] sm:tracking-[0.12em] md:tracking-[0.15em]"
+                    className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-light text-[var(--text-color)] mb-3 tracking-[0.10em] sm:tracking-[0.12em] md:tracking-[0.15em]"
                 >
-                    <span className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-normal text-[var(--maintext-color)] tracking-[0.15em] md:tracking-[0.20em] mt-8">
+                    <span className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-normal text-[var(--maintext-color)] tracking-[0.12em] md:tracking-[0.20em] mt-8">
                         ABOUT
                     </span>
                 </motion.p>
@@ -27,7 +27,7 @@ const About = () => {
                     whileInView={{ opacity: 1, y: 0 }}
                     viewport={{ once: false, amount: 0.4 }}
                     transition={{ duration: 0.6, delay: 0.2, ease: "easeOut" }}
-                    className="mt-6 sm:mt-8 text-lg sm:text-xl md:text-2xl lg:text-4xl leading-relaxed sm:leading-8 md:leading-10 lg:leading-[3rem] tracking-wide text-[var(--text-color)]"
+                    className="mt-6 sm:mt-8 text-base sm:text-lg md:text-2xl lg:text-4xl leading-relaxed sm:leading-7 md:leading-10 lg:leading-[3rem] tracking-wide text-[var(--text-color)]"
                 >
                     I’m Shreyas Karkera, a passionate web developer who loves creating clean,
                     modern, and responsive websites.
